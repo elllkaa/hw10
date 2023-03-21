82914a6283f2cea0f834e8bedd7d978c872ea01a
Пользователь <d.elllka@bk.ru>
2023-03-21 11:58:59 +0300

    hw

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника —  " + fullName);
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —" + fullName1);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
+
+    }
+}
